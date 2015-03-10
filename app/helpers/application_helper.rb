291688dcb35330ad2b21eef4d8f85ddb98f2417e
porterhaus <bporter@porterhaus.com>
2015-03-10 18:14:56 -0500

    More devise configuration.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  # Fix to get devise forms working within the application scope and not just the gem:
+  # http://pupeno.com/2010/08/29/show-a-devise-log-in-form-in-another-page/
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def day
+    @day = Time.now
+    @day.strftime('%A')
+  end
+
 end
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -163,4 +163,7 @@ SimpleForm.setup do |config|
 
   # Defines which i18n scope will be used in Simple Form.
   # config.i18n_scope = 'simple_form'
+
+  # Remove the asterick from required field labels
+  config.label_text = lambda { |label, required, explicit_label| "#{label}" }
 end
