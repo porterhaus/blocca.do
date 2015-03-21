e69219d11b683bbe1082ab5bb81b9a8adc1e4dac
porterhaus <bporter@porterhaus.com>
2015-03-21 12:49:59 -0500

    Change links for navigation.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
     if resource.save
-      # insert redirect_to logic -> profile page
+      set_flash_message :notice, :signed_up if is_flashing_format?
+      sign_up(resource_name, resource)
+      respond_with resource, location: after_sign_up_path_for(resource)
     else
       clean_up_passwords resource
       render pages_index_path
