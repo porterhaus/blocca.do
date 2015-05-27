f82fe08ff073fd5e7c4373b4c97f61c5ed64b78d
porterhaus <bporter@porterhaus.com>
2015-05-27 12:52:53 -0500

    Fixed migration name for Heroku.

--- db/migrate/20150327192843_add_auth_token_flag_to_users.rb
+++ db/migrate/20150327192843_add_auth_token_flag_to_users.rb
@@ -1,4 +1,4 @@
-class AddAuthTokenFlasToUsers < ActiveRecord::Migration
+class AddAuthTokenFlagToUsers < ActiveRecord::Migration
   def change
     add_column :users, :auth_token_flag, :boolean
   end
